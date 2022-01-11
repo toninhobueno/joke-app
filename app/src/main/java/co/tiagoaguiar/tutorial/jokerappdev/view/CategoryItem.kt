8ee723c6f5f8fd40ad7178e718eb2a0936c2b1c7
tonin <antonio.leite00@hotmail.com>
2022-01-10 23:32:49 -0300

    Recycler view feita, nova dependecia para clean code evitando varios adapters

---
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/CategoryItem.kt
@@ -0,0 +1,28 @@
+package co.tiagoaguiar.tutorial.jokerappdev.view
+
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import co.tiagoaguiar.tutorial.jokerappdev.R
+import co.tiagoaguiar.tutorial.jokerappdev.model.Category
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+
+class CategoryItem (private val category: Category) : Item<CategoryItem.CategoryViewHolder>() {
+
+    class CategoryViewHolder (view: View) : GroupieViewHolder(view)
+
+    override fun createViewHolder(itemView: View): CategoryViewHolder {
+        return CategoryViewHolder(itemView)
+    }
+
+    override fun bind(viewHolder: CategoryViewHolder, position: Int) {
+        viewHolder.itemView.findViewById<TextView>(R.id.txt_category).text = category.name
+        viewHolder.itemView.findViewById<LinearLayout>(R.id.container_category).setBackgroundColor(category.bgColor.toInt())
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.item_category
+    }
+
+}
